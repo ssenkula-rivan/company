867dcbd515f7b1891018bf258d3e749739b774ed
Ssenkula Rivan <cranictech@gmail.com>
2026-05-05 01:16:01 +0300

    Force Vercel redeploy - comment section is ready

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion'
 import { useState } from 'react'
 
+// Testimonials component with client review submission form
 export default function Testimonials() {
   const [commentData, setCommentData] = useState({
     name: '',
